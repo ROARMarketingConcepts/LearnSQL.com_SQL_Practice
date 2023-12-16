3999024db209af1bb8dbae66b2a3f0cb7dd82f7a
Ken Wood <ken@roarmarketingconcepts.com>
2023-12-15 20:21:08 -0500

    updated files

--- CASE Statements/Ratios_CASE_Statements.sql
+++ CASE Statements/Ratios_CASE_Statements.sql
@@ -22,3 +22,31 @@ SELECT
     ROUND(CAST(COUNT(CASE WHEN discontinued='t' THEN discontinued ELSE NULL END) AS decimal)/
     	COUNT(discontinued)*100,2) AS percentage_discontinued
 FROM products
+
+
+
+-- Create a report to show the percentage of total revenue after discount generated 
+-- by orders with low (less than or equal to 90.0) and high (greater than 90.0) freight 
+-- values in each country we ship to.
+
+-- Show the following columns: ship_country, percentage_low_freight, and percentage_high_freight.
+-- Round the percentages to two decimal places.
+
+WITH freight_revenue_by_country AS
+
+(SELECT ship_country,
+	CASE WHEN freight<=90 THEN SUM(quantity*unit_price*(1-discount)) ELSE 0 END AS low_freight_revenue,
+    CASE WHEN freight>90 THEN SUM(quantity*unit_price*(1-discount)) ELSE 0 END AS high_freight_revenue,
+    (CASE WHEN freight<=90 THEN SUM(quantity*unit_price*(1-discount)) ELSE 0 END) + 
+    (CASE WHEN freight>90 THEN SUM(quantity*unit_price*(1-discount)) ELSE 0 END) AS total_revenue
+FROM order_items oi
+LEFT JOIN orders o
+ON oi.order_id=o.order_id
+GROUP BY 1, o.freight)
+
+SELECT 
+	ship_country, 
+    ROUND(SUM(low_freight_revenue)/SUM(total_revenue)*100,2) AS percentage_low_freight,
+    ROUND(SUM(high_freight_revenue)/SUM(total_revenue)*100,2) AS percentage_high_freight
+FROM freight_revenue_by_country
+GROUP BY 1
