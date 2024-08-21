467a4e7086f59bbe057039f7b2c49df83b1a2d60
Ken Wood <ken@roarmarketingconcepts.com>
2024-08-20 20:40:58 -0400

    modified:   .DS_Store 	modified:   JOIN Nuances Self JOINs Unequal JOINs/UNION_EXCEPT_INTERSECT.sql

--- JOIN Nuances Self JOINs Unequal JOINs/UNION_EXCEPT_INTERSECT.sql
+++ JOIN Nuances Self JOINs Unequal JOINs/UNION_EXCEPT_INTERSECT.sql
@@ -13,3 +13,35 @@ FROM skating
 SELECT year FROM skating 
 MINUS
 SELECT year FROM cycling
+
+-- Display students' first and last names. Select only those students who meet BOTH of the following criteria:
+
+-- At least 3 of their final grades are higher than their midterm grades, and
+-- Their average final grade in the Mathematics learning path was at least 50.
+
+SELECT 
+	first_name,
+    last_name
+FROM course_enrollment cen
+JOIN student s
+ON cen.student_id = s.id
+GROUP BY 1,2
+HAVING SUM(CASE WHEN final_grade-midterm_grade > 0 THEN 1 ELSE 0 END) >= 3
+
+INTERSECT
+
+SELECT 
+	first_name,
+    last_name
+FROM course_enrollment cen
+JOIN student s
+ON cen.student_id = s.id
+JOIN course_edition ce
+ON cen.course_edition_id = ce.id
+JOIN course c
+ON ce.course_id = c.id
+WHERE learning_path = 'Mathematics'
+GROUP BY 1,2
+HAVING AVG(final_grade) >= 50
+
+
